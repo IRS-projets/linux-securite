90660e660cdb23cf8d76e6ad0ba9ba3a1e496dc1
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2022-11-17 16:33:58 +0100

    Update script.sh
     - Expliciter commentaires
     - Obtenir valeur memoire
     - Obtenir valeur disque
     - Ecrire log dans fichier
     - Tronquer fichier pour qu'il soir a longueur fixe

--- script-tim/script.sh
+++ script-tim/script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# - Récupéreer les infos date, processeur, mémoire et disque
-# - Les append dans un fichier de log avec la date
-# - Supprimer les lignes en trop
+# Fichier contenant les releves
+#file="/var/www/html/projetlinux.com/database"
+file="temp"
 
-file="/var/www/html/projetlinux.com/database"
+### Recueil et conversion en % des informations
+# Date au format hh:mm:ss
+date=$(date +"%T")
+# Processeur: 100 - taux de repos
+processor=$(mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }')
+# Memoire disponible
+memoryTotal=$(free | awk 'NR==2{ print $2}')
+# Memoire utilisee
+memoryUsed=$(free | awk 'NR==2{ print $3}')
+# Memoire en %
+memory=$((memoryUsed*100/memoryTotal))
+# Disque: volume utilise
+disk=$(df -t ext4 | awk 'NR==2{ print $5}' | grep -Eo '[0-9]{1,3}') 
 
-# Recceil et conversion en % des informations
-date=date +"%T"
-processor=mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }'
-memory=free -m used*100/total
-disk=df -t ext4 
-
-# Ecriture de la ligne de log
-"${date};${processor};${memory};${disk}" >> file
+### Ecriture du fichier
+# Ligne de log
+echo "${date};${processor};${memory};${disk}" >> $file
 # Troncature du fichier
-tail -n 120 file >> file
+echo "$(tail -n 120 $file)" > $file
