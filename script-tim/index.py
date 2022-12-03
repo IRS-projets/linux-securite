1264c4587649fcbfee5327c7aaec6f1afd797aaf
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2022-12-04 00:17:57 +0100

    Update index.py
     - Supprimer sommaire
     - Ajouter variables
     - Implémenter copie des valeurs du fichier vers un tableau
     - Implémenter suppression des valuers datant de plus d'1h
     - Implémenter ajout de "trous" pour les mesures non-consécutives
     - Implémenter ajout des valeurs mesurées dans les tableaux d'affichage

--- script-tim/index.py
+++ script-tim/index.py
@@ -2,35 +2,55 @@
 
 import time
 
-# Lire une ligne
-# Séparer les valeurs
-# Mettre les "trous"
-# Remplir les tablaux d'affichage
-
+### Definitions
+# No_data value
 no_data = -1
+# Arrays of values that will be displayed
 processor = [no_data]*60
 memory = [no_data]*60
 disk = [no_data]*60
+# Counter
+counter = 0
+# Index of the measure inside the displayed arrays
+index = 0
+# Table that will have all the measures and timestamps
+# Format timetamp;processor;memory;disk
+# Timestamp is in seconds since Epoch, the rest in used %
+measures = list(tuple())
+# Total quantity of measures taken
+measures_lenth = 0
 
-counter = 60
+### Copying measures to table
 with open("fichier", 'r') as file:
-    for line in reversed(list(file)):
+    for line in file:
         content = line.split(';')
-        time_of_measure = content[0]
-        processor_value = content[1]
-        memory_value = content[2]
-        disk_value = content[3]
-        if line == 1:
-            y = 0
-            while int(time.time()) > time_of_measure+60*y:
-                y += 1
-                counter -= 1
-        while time_of_measure < ((line-1).split(';')[0]) - 30:
-            counter -= 1
-        processor[counter] = processor_value
-        memory[counter] = memory_value
-        disk[counter] = disk_value
-        counter -= 1
+        measures[measures_lenth][0] = content[0]
+        measures[measures_lenth][1] = content[1]
+        measures[measures_lenth][2] = content[2]
+        measures[measures_lenth][3] = content[3]
+        measures_lenth += 1
+
+### "Deleting" measures that are older than 1 hour
+while measures[counter][0] < int(time.time())-60*60:
+    counter +=1
+
+### Skipping display slots until we find the oldest measure that is least than 1h old
+skipped_counter = 0
+while measures[counter][0] > int(time.time())-60*60+skipped_counter*60:
+    index += 1
+    counter += 1
+
+###  Assigning values to the display arrays
+for x in range(counter, measures_lenth):
+    # Adding "holes" if measures where skipped
+    if x > counter:
+        y = 0
+        while measures[x][0] > measures[x-1][0] + 60 + 60*y:
+            y += 1
+            index += 1
+    processor[index] = measures[x][1]
+    memory[index] = measures[x][2]
+    disk[index] = measures[x][3]
 
 print(processor)
 print(memory)
