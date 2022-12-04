79a4423261208a46b26ff92b3c6f20d9fa368f59
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2022-12-04 21:57:35 +0100

    Update index.py
    Ajouter affichage HTML

--- script-tim/index.py
+++ script-tim/index.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python
 
-import time
+#import time
 
-### Definitions
+def Draw_Graph(values, type):
+    max_height = 100
+    if type == "processor":
+        color = ["lightblue", "blue"]
+    if type == "memory":
+        color = ["pink", "purple"]
+    if type == "disk":
+        color = ["lightgreen", "green"]
+    for x in range(60):
+        value = values[x]
+        if value == no_data:
+            print('''div class="no_data"></div>''')
+        if value == 0:
+            print('''<div class="bar" style="height: 0px; margin-top: ''' +
+                  max_height + '''px;">''')
+        if value > 0:
+            height = (index/100) * max_height
+            margin = max_height - height
+            print('''<div class="bar" style="height:''' + str(height) + '''px; margin-top: ''' +
+                  str(margin) + '''px; background: ''' + str(color[0]) + '''; border-color: ''' + str(color[1]) + '''">''')
+
+# Definitions
 # No_data value
 no_data = -1
 # Arrays of values that will be displayed
@@ -18,15 +39,16 @@ measures = list()
 # Total quantity of measures taken
 measures_quantity = 0
 
-### Copying measures to table
+# Copying measures to table
 with open("fichier", 'r') as file:
     for line in file:
         content_txt = line.split(';')
-        content_int = [int(content_txt[0]), int(content_txt[1]),int(content_txt[2]),int(content_txt[3])]
+        content_int = [int(content_txt[0]), int(content_txt[1]),
+                       int(content_txt[2]), int(content_txt[3])]
         measures.append(content_int)
         measures_quantity += 1
 
-###  Assigning values to the display arrays
+# Assigning values to the display arrays
 for x in range(0, measures_quantity):
     # Adding "holes" if measures where skipped
     if x > 0:
@@ -38,3 +60,40 @@ for x in range(0, measures_quantity):
     memory[index] = measures[x][2]
     disk[index] = measures[x][3]
     index += 1
+
+print("Content-type: text/html\n\n")
+print('''
+<!DOCTYPE html>
+
+<head>
+    <title>
+        Performances
+    </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="winwow">
+        <div class="window_header">
+            <button class="custom_button">Reload</button>
+        </div>
+        <div class="window_content">
+            <div class="metric">
+                Processor: X%
+                <div class="graph">
+                ''' + Draw_Graph(processor, "processor") + '''</div>
+            </div>
+            <div class="metric">
+                Memory: X%
+                <div class="graph">
+                ''' + Draw_Graph(memory, "memory") + '''</div>
+            </div>
+            <div class="metric">
+                Disk: X%
+                <div class="graph">
+                ''' + Draw_Graph(disk, "disk") + '''</div>
+            </div>
+        </div>
+    </div>
+</body>
+''')
