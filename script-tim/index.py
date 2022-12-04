687909bcc9889468f143dc7af1116a7bc1eb98ee
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2022-12-04 20:30:39 +0100

    Update index.py
     - Simplification du transfert des données du fichier vers la table
     - Supression des contrôles d'âge des mesures (non fonctionnel et manque de temps de les dev d'aventage)
     - Suppression des affichages à la fin

--- script-tim/index.py
+++ script-tim/index.py
@@ -9,41 +9,27 @@ no_data = -1
 processor = [no_data]*60
 memory = [no_data]*60
 disk = [no_data]*60
-# Counter
-counter = 0
 # Index of the measure inside the displayed arrays
 index = 0
 # Table that will have all the measures and timestamps
 # Format timetamp;processor;memory;disk
 # Timestamp is in seconds since Epoch, the rest in used %
-measures = [[no_data]*4]*60
+measures = list()
 # Total quantity of measures taken
 measures_quantity = 0
 
 ### Copying measures to table
 with open("fichier", 'r') as file:
     for line in file:
-        content = line.split(';')
-        measures[measures_quantity][0] = content[0]
-        measures[measures_quantity][1] = content[1]
-        measures[measures_quantity][2] = content[2]
-        measures[measures_quantity][3] = content[3]
+        content_txt = line.split(';')
+        content_int = [int(content_txt[0]), int(content_txt[1]),int(content_txt[2]),int(content_txt[3])]
+        measures.append(content_int)
         measures_quantity += 1
 
-### "Deleting" measures that are older than 1 hour
-while measures[counter][0] < int(time.time())-60*60:
-    counter +=1
-
-### Skipping display slots until we find the oldest measure that is least than 1h old
-skipped_counter = 0
-while measures[counter][0] > int(time.time())-60*60+skipped_counter*60 and 60-index > measures_quantity:
-    index += 1
-    counter += 1
-
 ###  Assigning values to the display arrays
-for x in range(counter, measures_quantity):
+for x in range(0, measures_quantity):
     # Adding "holes" if measures where skipped
-    if x > counter:
+    if x > 0:
         y = 0
         while measures[x][0] > measures[x-1][0] + 60 + 60*y:
             y += 1
@@ -51,7 +37,4 @@ for x in range(counter, measures_quantity):
     processor[index] = measures[x][1]
     memory[index] = measures[x][2]
     disk[index] = measures[x][3]
-
-print(processor)
-print(memory)
-print(disk)
+    index += 1
